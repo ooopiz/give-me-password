25fc857efb2c285d419f02512fb8853c2cd39a24
ooopiz <r.huang850@gmail.com>
2020-06-16 02:48:06 +0800

    Fix main.go

--- main.go
+++ main.go
@@ -1,41 +1,22 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
-var (
-	w    int
-	n    int
-	v, V bool
-	c    string
-)
-
-func init() {
-
-	flag.IntVar(&w, "w", 12, "Set `word` count")
-
-	flag.BoolVar(&v, "v", false, "Show version then exit")
-	flag.BoolVar(&V, "V", false, "Show version and configure options then exit")
-
-	flag.Parse()
-
-	if v {
-		fmt.Println("Version 1.0.0")
-		os.Exit(0)
-	}
-	if V {
-		fmt.Println("Version 1.0.0 ...")
-		os.Exit(0)
-	}
-}
-
 func main() {
+	var w int
+	if len(os.Args) < 2 {
+		w = 12
+	} else {
+		w, _ = strconv.Atoi(os.Args[1])
+	}
+
 	//  number := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 	//  capitalLetter := []string{
 	//      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
